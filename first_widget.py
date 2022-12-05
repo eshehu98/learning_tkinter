dbef96256e774d0f1f474283dd6032debacf6354
eshehu98 <eshehu@fordham.edu>
2022-12-05 18:07:15 -0500

    first example

--- first_widget.py
+++ first_widget.py
@@ -1,3 +1,14 @@
-#doing hello world at first
-print("hello world")
+from tkinter import *
+from tkinter.ttk import *
 
+# create the main window object named root
+root = Tk()
+
+# titling the main window object
+root.title("First_Program")
+
+# the label "widget" is what the label will be on the screen
+label = Label(root, text = "hello world").pack()
+
+# the following function call will telll the code to keep displaying the program
+root.mainloop()
