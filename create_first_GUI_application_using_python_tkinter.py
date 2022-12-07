79490446c124d98616f30888b3100308bc69639d
eshehu98 <eshehu@fordham.edu>
2022-12-07 18:13:49 -0500

    first GUI app

---
+++ create_first_GUI_application_using_python_tkinter.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+
+root = Tk()
+
+
+root.title("Welcome to GeekForGeeks")
+root.geometry("300x250")
+
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label = "New")
+menu.add_cascade(label = "File", menu = item)
+root.config(menu = menu)
+
+
+lbl = Label(root, text = "Are you a Geek?")
+lbl.grid()
+
+txt = Entry(root, width = 10)
+txt.grid(column = 1, row = 0)
+
+
+def clicked():
+    res = "You wrote" + txt.get()
+    lbl.configure(text = res)
+
+btn = Button(root, text = "Click me", fg = "red", command = clicked)
+
+btn.grid(column = 2, row = 0)
+
+
+root.mainloop()
+
